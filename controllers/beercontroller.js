7d18e9fe63ff26cca187ec5145a626992e950280
Denis DelGrosso <denis@kargo.com>
2017-06-09 12:03:36 -0400

    Promises

--- controllers/beercontroller.js
+++ controllers/beercontroller.js
@@ -13,29 +13,77 @@ class BeerController {
     if (req.body.token === this.config.slacktoken) {
       const tokens = req.body.text.split(' ');
       if (tokens.length > 0 && tokens[0] === 'fav') {
-        this.untappdClient.userDistinctBeers((err, obj) => {
-          const resp = this.handleBeerSearch(err, obj);
-          res.send(resp);
-        }, {USERNAME: tokens[1], sort: 'checkin', limit: 1});
+        this.getFavorites(tokens[1])
+        .then((result) => {
+          const response = this.handleBeerSearch(result);
+          res.send(response);
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
       } else if (tokens.length > 0 && tokens[0] === 'badge') {
-        this.untappdClient.userBadges((err, obj) => {
-          const resp = this.handleUserBadge(err, obj);
-          res.send(resp);
-        }, {USERNAME: tokens[1], limit: 1});
+        this.getBadges(tokens[1])
+        .then((result) => {
+          const response = this.handleUserBadge(result);
+          res.send(response);
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
       } else {
-        this.untappdClient.beerSearch((err, obj) => {
-          const resp = this.handleBeerSearch(err, obj);
-          res.send(resp);
-        }, {q: req.body.text, sort: 'count'});
+        this.beerSearch(req.body.text)
+        .then((result) => {
+          const response = this.handleBeerSearch(result);
+          res.send(response);
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
       }
     } else {
       res.status(500).send('Invalid Token');
     }
   }
 
-  handleBeerSearch(err, obj) {
+  beerSearch(text) {
+    return new Promise((resolve, reject) => {
+      this.untappdClient.beerSearch((err, obj) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(obj);
+        }
+      }, {q: text, sort: 'count'});
+    });
+  }
+
+  getBadges(username) {
+    return new Promise((resolve, reject) => {
+      this.untappdClient.userBadges((err, obj) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(obj);
+        }
+      }, {USERNAME: username, limit: 1});
+    });
+  }
+
+  getFavorites(username) {
+    return new Promise((resolve, reject) => {
+      this.untappdClient.userDistinctBeers((err, obj) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(obj);
+        }
+      }, {USERNAME: username, sort: 'checkin', limit: 1});
+    });
+  }
+
+  handleBeerSearch(obj) {
     const response = {attachments: []};
-    if (err === null && obj.response.beers.count > 0) {
+    if (obj.response.beers.count > 0) {
       const beer = obj.response.beers.items[0].beer;
       const brewery = obj.response.beers.items[0].brewery;
       const count = obj.response.beers.items[0].count;
@@ -67,9 +115,9 @@ class BeerController {
     return response;
   }
 
-  handleUserBadge(err, obj) {
+  handleUserBadge(obj) {
     const response = {attachments: []};
-    if (err === null && obj.response.count > 0) {
+    if (obj.response.count > 0) {
       const badge = obj.response.items[0];
       response.response_type = 'in_channel';
       const attachment = {
